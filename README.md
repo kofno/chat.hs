2d16fe052c99f1e8a84fa3f879c08f3b37937b3a
Ryan L. Bell <ryan.l.bell@gmail.com>
2013-09-14 20:59:00 -0400

    Initial commit. This is the work from the book
    Will perform the exercises from here.

---
+++ README.md
@@ -0,0 +1,8 @@
+This extends the Chat Service from Parallel and Concurrent Programming in
+Haskell.
+
+The book recommends the following exercises:
+
+  * Replace the expensive broadcast STM operation with something more
+    performant. Suggestion: a broadcast channel.
+
---
+++ chat.hs
@@ -0,0 +1,191 @@
+{-# LANGUAGE RecordWildCards #-}
+module Main where
+
+import System.IO
+import Network
+import Text.Printf
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Concurrent.Async
+import Control.Exception.Base
+import Control.Monad
+
+-- <<main
+main :: IO ()
+main = withSocketsDo $ do
+  server <- newServer
+  sock   <- listenOn (PortNumber (fromIntegral port))
+  printf "Listening on port %d\n" port
+  forever $ do
+    (handle, host, port) <- accept sock
+    printf "Accepted connection from %s:%s\n" host (show port)
+    forkFinally (talk handle server) (\_ -> hClose handle)
+
+
+port :: Integer
+port = 44444
+-- >>
+
+
+-- <<Client
+type ClientName = String
+
+data Client = Client
+  { clientName     :: ClientName
+  , clientHandle   :: Handle
+  , clientKicked   :: TVar (Maybe String)
+  , clientSendChan :: TChan Message
+  }
+
+newClient :: ClientName -> Handle -> STM Client
+newClient name handle = do
+  c <- newTChan
+  k <- newTVar Nothing
+  return Client { clientName     = name
+                , clientHandle   = handle
+                , clientKicked   = k
+                , clientSendChan = c
+                }
+-- >>
+
+-- <<Server
+data Server = Server
+  { clients :: TVar (Map ClientName Client)
+  }
+
+newServer :: IO Server
+newServer = do
+  c <- newTVarIO Map.empty
+  return Server { clients = c }
+-- >>
+
+-- <<Message
+data Message = Notice String
+             | Tell ClientName String
+             | Broadcast ClientName String
+             | Command String
+-- >>
+
+-------------------------------------------------------------------------------
+-- Basic ops
+
+broadcast :: Server -> Message -> STM ()
+broadcast Server{..} msg = do
+  clientmap <- readTVar clients
+  mapM_ (\client -> sendMessage client msg) (Map.elems clientmap)
+
+tell :: Server -> Client -> ClientName -> String -> IO ()
+tell server@Server{..} Client{..} who msg = do
+  ok <- atomically $ sendToName server who (Tell clientName msg)
+  if ok
+     then return ()
+     else hPutStrLn clientHandle (who ++ " is not connected.")
+
+kick :: Server -> ClientName -> ClientName -> STM ()
+kick server@Server{..} who by = do
+  clientmap <- readTVar clients
+  case Map.lookup who clientmap of
+       Nothing ->
+         void $ sendToName server by (Notice $ who ++ " is not connected")
+       Just victim -> do
+         writeTVar (clientKicked victim) $ Just ("by " ++ by)
+         void $ sendToName server by (Notice $ "you kicked " ++ who)
+
+sendMessage :: Client -> Message -> STM ()
+sendMessage Client{..} msg =
+  writeTChan clientSendChan msg
+
+sendToName :: Server -> ClientName -> Message -> STM Bool
+sendToName server@Server{..} name msg = do
+  clientmap <- readTVar clients
+  case Map.lookup name clientmap of
+       Nothing -> return False
+       Just client -> sendMessage client msg >> return True
+
+
+--------------------------------------------------------------------------------
+-- Main server ops
+
+checkAddClient :: Server -> ClientName -> Handle -> IO (Maybe Client)
+checkAddClient server@Server{..} name handle = atomically $ do
+  clientmap <- readTVar clients
+  if Map.member name clientmap
+     then return Nothing
+     else do
+       client <- newClient name handle
+       writeTVar clients $ Map.insert name client clientmap
+       broadcast server $ Notice (name ++ " has connected")
+       return (Just client)
+
+removeClient :: Server -> ClientName -> IO ()
+removeClient server@Server{..} name = atomically $ do
+  modifyTVar' clients $ Map.delete name
+  broadcast server $ Notice (name ++ " has disconnected")
+
+runClient :: Server -> Client -> IO ()
+runClient serv@Server{..} client@Client{..} = do
+  race server receive
+  return ()
+  where
+    receive = forever $ do
+      msg <- hGetLine clientHandle
+      atomically $ sendMessage client (Command msg)
+
+    server = join $ atomically $ do
+      k <- readTVar clientKicked
+      case k of
+           Just reason -> return $
+             hPutStrLn clientHandle $ "You have been kicked: " ++ reason
+           Nothing -> do
+             msg <- readTChan clientSendChan
+             return $ do
+               continue <- handleMessage serv client msg
+               when continue $ server
+
+handleMessage :: Server -> Client -> Message -> IO Bool
+handleMessage server client@Client{..} message =
+  case message of
+       Notice msg         -> output $ "*** " ++ msg
+       Tell name msg      -> output $ "*" ++ name ++ "*: " ++ msg
+       Broadcast name msg -> output $ "<" ++ name ++ ">: " ++ msg
+       Command msg ->
+         case words msg of
+              ["/kick", who] -> do
+                atomically $ kick server who clientName
+                return True
+              "/tell" : who : what -> do
+                tell server client who (unwords what)
+                return True
+              ["/quit"] ->
+                return False
+              ('/':_):_ -> do
+                hPutStrLn clientHandle $ "Unrecognized command: " ++ msg
+                return True
+              _ -> do
+                atomically $ broadcast server $ Broadcast clientName msg
+                return True
+  where
+    output s = do hPutStrLn clientHandle s; return True
+
+
+talk :: Handle -> Server -> IO ()
+talk handle server@Server{..} = do
+  hSetNewlineMode handle universalNewlineMode
+  hSetBuffering handle LineBuffering
+  readName
+  where
+    readName = do
+      hPutStrLn handle "What is your name?"
+      name <- hGetLine handle
+      if null name
+         then readName
+         else mask $ \restore -> do
+               ok <- checkAddClient server name handle
+               case ok of
+                    Nothing -> restore $ do
+                      hPrintf handle "The name %s is in use. Please choose another\n" name
+                      readName
+                    Just client -> restore (runClient server client)
+                                     `finally` removeClient server name
